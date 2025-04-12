f3e4cc2777c745e167b0ebd5ab0161ce71cf9661
IvoMart <ivo_ivancho@hotmail.com>
2025-04-12 14:55:37 -0300

    metricas - grafana y  prometeus

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong1' });
 });
+
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
 
 if (require.main === module) {
   app.listen(port, () => {
